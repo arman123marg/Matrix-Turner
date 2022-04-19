0d435661edd0df83443ddd65e48f03900aa39f80
arman123marg <arman.m.mic@outlook.com>
2022-04-19 18:52:36 +0400

    ful Project

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgBaseMatrix = new System.Windows.Forms.DataGridView();
+            this.txtMatrixI = new System.Windows.Forms.TextBox();
+            this.txtMatrixJ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerateMatrix = new System.Windows.Forms.Button();
+            this.dgTurnedMatrix = new System.Windows.Forms.DataGridView();
+            this.cbChooseDegree = new System.Windows.Forms.ComboBox();
+            this.btnTurn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBaseMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTurnedMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgBaseMatrix
+            // 
+            this.dgBaseMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBaseMatrix.Location = new System.Drawing.Point(13, 13);
+            this.dgBaseMatrix.Name = "dgBaseMatrix";
+            this.dgBaseMatrix.RowHeadersWidth = 51;
+            this.dgBaseMatrix.RowTemplate.Height = 24;
+            this.dgBaseMatrix.Size = new System.Drawing.Size(449, 450);
+            this.dgBaseMatrix.TabIndex = 0;
+            // 
+            // txtMatrixI
+            // 
+            this.txtMatrixI.Location = new System.Drawing.Point(531, 44);
+            this.txtMatrixI.Name = "txtMatrixI";
+            this.txtMatrixI.Size = new System.Drawing.Size(100, 22);
+            this.txtMatrixI.TabIndex = 1;
+            // 
+            // txtMatrixJ
+            // 
+            this.txtMatrixJ.Location = new System.Drawing.Point(531, 83);
+            this.txtMatrixJ.Name = "txtMatrixJ";
+            this.txtMatrixJ.Size = new System.Drawing.Size(100, 22);
+            this.txtMatrixJ.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(543, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Matrix Sizes";
+            // 
+            // btnGenerateMatrix
+            // 
+            this.btnGenerateMatrix.Location = new System.Drawing.Point(531, 128);
+            this.btnGenerateMatrix.Name = "btnGenerateMatrix";
+            this.btnGenerateMatrix.Size = new System.Drawing.Size(100, 41);
+            this.btnGenerateMatrix.TabIndex = 4;
+            this.btnGenerateMatrix.Text = "Generate Matrix";
+            this.btnGenerateMatrix.UseVisualStyleBackColor = true;
+            this.btnGenerateMatrix.Click += new System.EventHandler(this.btnGenerateMatrix_Click);
+            // 
+            // dgTurnedMatrix
+            // 
+            this.dgTurnedMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTurnedMatrix.Location = new System.Drawing.Point(699, 13);
+            this.dgTurnedMatrix.Name = "dgTurnedMatrix";
+            this.dgTurnedMatrix.RowHeadersWidth = 51;
+            this.dgTurnedMatrix.RowTemplate.Height = 24;
+            this.dgTurnedMatrix.Size = new System.Drawing.Size(449, 450);
+            this.dgTurnedMatrix.TabIndex = 5;
+            // 
+            // cbChooseDegree
+            // 
+            this.cbChooseDegree.FormattingEnabled = true;
+            this.cbChooseDegree.Location = new System.Drawing.Point(531, 272);
+            this.cbChooseDegree.Name = "cbChooseDegree";
+            this.cbChooseDegree.Size = new System.Drawing.Size(100, 24);
+            this.cbChooseDegree.TabIndex = 6;
+            // 
+            // btnTurn
+            // 
+            this.btnTurn.Location = new System.Drawing.Point(531, 327);
+            this.btnTurn.Name = "btnTurn";
+            this.btnTurn.Size = new System.Drawing.Size(100, 26);
+            this.btnTurn.TabIndex = 7;
+            this.btnTurn.Text = "Turn";
+            this.btnTurn.UseVisualStyleBackColor = true;
+            this.btnTurn.Click += new System.EventHandler(this.btnTurn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1212, 687);
+            this.Controls.Add(this.btnTurn);
+            this.Controls.Add(this.cbChooseDegree);
+            this.Controls.Add(this.dgTurnedMatrix);
+            this.Controls.Add(this.btnGenerateMatrix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMatrixJ);
+            this.Controls.Add(this.txtMatrixI);
+            this.Controls.Add(this.dgBaseMatrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgBaseMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTurnedMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgBaseMatrix;
+        private System.Windows.Forms.TextBox txtMatrixI;
+        private System.Windows.Forms.TextBox txtMatrixJ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerateMatrix;
+        private System.Windows.Forms.DataGridView dgTurnedMatrix;
+        private System.Windows.Forms.ComboBox cbChooseDegree;
+        private System.Windows.Forms.Button btnTurn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        int[,] matrix;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbChooseDegree.Items.Add("turn 90° right");
+            cbChooseDegree.Items.Add("turn 90° left");
+            cbChooseDegree.Items.Add("turn 180°");
+
+        }
+
+        private void btnGenerateMatrix_Click(object sender, EventArgs e)
+        {
+            int matrixI = int.Parse(txtMatrixI.Text);
+            int matrixJ = int.Parse(txtMatrixJ.Text);
+            matrix = new int[matrixI, matrixJ];
+            MatrixGenerator();
+            PrintInDgView(matrix, dgBaseMatrix);
+
+        }
+        void MatrixGenerator()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(-10, 11);
+                }
+            }
+        }
+        static void PrintInDgView(int[,] matrix, DataGridView dataGridView)
+        {
+            dataGridView.RowCount = matrix.GetLength(0);
+            dataGridView.ColumnCount = matrix.GetLength(1);
+            for (int i = 0; i < dataGridView.ColumnCount; i++)
+            {
+                dataGridView.Columns[i].Width = 25;
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    dataGridView.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+        }
+
+        private void btnTurn_Click(object sender, EventArgs e)
+        {
+            int[,] turnedMatrix;
+            switch (cbChooseDegree.SelectedIndex)
+            {
+                case 0:
+                    turnedMatrix = TurnIn90();
+                    break;
+                case 1:
+                    turnedMatrix = TurnIn270();
+                    break;
+                case 2:
+                    turnedMatrix = TurnIn180();
+                    break;
+                default:
+                    MessageBox.Show("Choose degree");
+                    return;
+
+            }
+            PrintInDgView(turnedMatrix, dgTurnedMatrix);
+        }
+        int[,] TurnIn90()
+        {
+            int[,] turnedMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    turnedMatrix[j, matrix.GetLength(0) - i - 1] = matrix[i, j];
+                }
+            }
+
+            return turnedMatrix;
+        }
+        int[,] TurnIn270()
+        {
+            int[,] turnedMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    turnedMatrix[matrix.GetLength(1) - j - 1, i] = matrix[i, j];
+                }
+            }
+
+            return turnedMatrix;
+        }
+        int[,] TurnIn180()
+        {
+            int[,] turnedMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    turnedMatrix[matrix.GetLength(0) - i - 1, matrix.GetLength(1) - j - 1] = matrix[i, j];
+                }
+            }
+
+            return turnedMatrix;
+        }
+    }
+}
